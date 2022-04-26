2213781280f5c7cefe3f4eea715f9446af85ebb2
stephanieopon <oponstephanie@gmail.com>
2022-04-26 16:18:46 -0500

    audio and haptics

--- Bowlingg/Assets/Scripts/BowlingBallController.cs
+++ Bowlingg/Assets/Scripts/BowlingBallController.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class BowlingBallController : MonoBehaviour
 {
+    public GameObject ball;
+    private Vector3 ballLoc;
+    public AudioClip ballSound;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ball = GameObject.Find("Bowling Ball");
+        ballLoc = ball.transform.position;
+        GetComponent<AudioSource>().clip = ballSound;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ballLoc = ball.transform.position;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "ground")
+        {
+            AudioSource.PlayClipAtPoint(ballSound, ballLoc);
+        }
     }
 }
--- Bowlingg/Assets/Scripts/PinController.cs
+++ Bowlingg/Assets/Scripts/PinController.cs
@@ -2,17 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PinController : MonoBehaviour
 {
+    public GameObject pin;
+    private Vector3 pinLoc;
+    public AudioClip pinSound;
     // Start is called before the first frame update
     void Start()
     {
-        
+        pin = GameObject.Find("Pin");
+        pinLoc = pin.transform.position;
+        GetComponent<AudioSource>().clip = pinSound;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        pinLoc = pin.transform.position;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Bowling Ball")
+        {
+            AudioSource.PlayClipAtPoint(pinSound, pinLoc);
+        }
     }
 }
